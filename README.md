8d006b0e1e8348aa6a29cfe4ee19ecdb6aeda117
Daniel Meijer <danielmeijer96@hotmail.com>
2020-05-16 21:46:21 +0200

    First commit here

---
+++ README.md
@@ -0,0 +1,7 @@
+# Plex Updater
+
+This script will update your Plex instance to the latest version. 
+
+### Requirements 
+
+- jq installed
---
+++ plex_updater.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -e
+
+### VARS
+
+PLEX_JSON_URL="https://plex.tv/pms/downloads/5.json"
+# Uncomment this and comment  if you're using PLEX PASS
+PLEX_JSON_URL="https://plex.tv/api/downloads/1.json?channel=plexpass"
+
+PLEX_URL=$(curl -s $PLEX_JSON_URL | jq '.computer.Linux.releases[] | select(.label=="Ubuntu (16.04+) / Debian (8+) - Intel/AMD 64-bit") | .url')
+TMP_DEB=/tmp/plex.deb
+
+
+### DOWNLOADING PLEX LATEST VERSION
+
+echo "Plex URL: $PLEX_URL"
+wget $PLEX_URL -O $TMP_DEB
+
+
+### Installing (if you're root :D)
+
+if [ `whoami` = root ] 
+then
+    dkpg -i $TMP_DEB
+else
+    echo "Are you root?"
+    echo "To install manually execute: dkpg -i $TMP_DEB"
+fi
